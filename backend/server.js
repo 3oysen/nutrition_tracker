cf97ca68553e7dbce0f5a7d4c36bbb561a721e0d
3oysen <103428192+3oysen@users.noreply.github.com>
2022-12-19 19:56:30 +0100

    deleted unused goals, added routing to frontend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { urrorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 5000;
+const port = process.env.BACKEND_PORT || 5001;
 
 connectDB();
 
@@ -12,7 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(urrorHandler);
