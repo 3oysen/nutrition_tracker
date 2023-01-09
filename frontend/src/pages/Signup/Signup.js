ba52fce6c931a0fe8ea49c0f140550a1bd7d06c1
3oysen <103428192+3oysen@users.noreply.github.com>
2023-01-09 19:55:02 +0100

    added validation to form

--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -1,9 +1,33 @@
 import RightPanel from "../../components/RightPanel";
 import { useFormik } from "formik";
 
+const validate = (values) => {
+	const errors = {};
+	if (!values.name) {
+		errors.name = "Required";
+	} else if (values.name.length > 15) {
+		errors.name = "Must be 15 characters or less";
+	}
+
+	if (!values.email) {
+		errors.email = "Required";
+	} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+		errors.email = "Invalid email address";
+	}
+
+	if (!values.password) {
+		errors.password = "Required";
+	} else if (values.password.length < 8) {
+		errors.password = "Must be 8 characters or more";
+	}
+
+	return errors;
+};
+
 const Signup = () => {
 	const formik = useFormik({
 		initialValues: { name: "", email: "", password: "" },
+		validate,
 		onSubmit: (values) => {
 			alert(JSON.stringify(values, null, 2));
 		},
@@ -26,18 +50,31 @@ const Signup = () => {
 								name="name"
 								type="text"
 								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
 								value={formik.values.name}
 								placeholder="Enter your name"
 							/>
+							<div className="form-errors">
+								{formik.touched.name && formik.errors.name ? (
+									<div>{formik.errors.name}</div>
+								) : null}
+							</div>
+
 							<h5>Email</h5>
 							<input
 								id="email"
 								name="email"
 								type="email"
 								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
 								value={formik.values.email}
 								placeholder="Enter your email"
 							/>
+							<div className="form-errors">
+								{formik.touched.email && formik.errors.email ? (
+									<div>{formik.errors.email}</div>
+								) : null}
+							</div>
 
 							<h5>Password</h5>
 							<input
@@ -45,9 +82,15 @@ const Signup = () => {
 								name="password"
 								type="password"
 								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
 								value={formik.values.password}
 								placeholder="Enter your password"
 							/>
+							<div className="form-errors">
+								{formik.touched.password && formik.errors.password ? (
+									<div>{formik.errors.password}</div>
+								) : null}
+							</div>
 
 							<div className="sign-button-section">
 								<button type="submit" class="sign-btn">
